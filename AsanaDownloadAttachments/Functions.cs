ebf6bb39bf467cb2d31e557d577246824d0a22d1
lordbohne <bohnelord@gmail.com>
2022-05-28 16:36:52 +0200

    Intital Commit

---
+++ AsanaDownloadAttachments/Functions.cs
@@ -0,0 +1,106 @@
+﻿namespace AsanaClient;
+
+public static class Functions
+{
+    public static ProjectCompact GetProjectToDownloadFrom(Response66 projects)
+    {
+        ProjectCompact? projectToDownload = null;
+        var projectList = projects.Data.ToList();
+        while (projectToDownload is null)
+        {
+            var userInput = Console.ReadLine();
+            if (!int.TryParse(userInput, out var index))
+            {
+                Console.WriteLine("Please enter a valid number.");
+                continue;
+            }
+            if (index > projectList.Count || index < 1)
+            {
+                Console.WriteLine($"Please enter a number in the range of {1} to {(projectList.Count - 1 < 1 ? 1 : projectList.Count)}");
+                continue;
+            }
+
+            projectToDownload = projectList[index - 1];
+        }
+
+        return projectToDownload;
+    }
+    
+    public static TaskCompact GetTaskToDownloadFrom(Response115 tasks)
+    {
+        TaskCompact? projectToDownload = null;
+        var taskList = tasks.Data.ToList();
+        while (projectToDownload is null)
+        {
+            var userInput = Console.ReadLine();
+            if (!int.TryParse(userInput, out var index))
+            {
+                Console.WriteLine("Please enter a valid number.");
+                continue;
+            }
+            if (index > taskList.Count || index < 1)
+            {
+                Console.WriteLine($"Please enter a number in the range of {1} to {(taskList.Count - 1 < 1 ? 1 : taskList.Count)}");
+                continue;
+            }
+
+            var project = taskList[index - 1];
+            projectToDownload = project;
+        }
+
+        return projectToDownload;
+    }
+
+    public static bool GetRepeatDecisionFromUser()
+    {
+        var userInput = Console.ReadLine();
+        return userInput?.ToLower() is "yes";
+    }
+    
+    public static async Task DownloadAsync(this HttpClient client, string requestUri, Stream destination, IProgress<float> progress = null, CancellationToken cancellationToken = default)
+    {
+        // Get the http headers first to examine the content length
+        using var response = await client.GetAsync(requestUri, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+        var contentLength = response.Content.Headers.ContentLength;
+
+        await using var download = await response.Content.ReadAsStreamAsync(cancellationToken);
+        
+        // Ignore progress reporting when no progress reporter was 
+        // passed or when the content length is unknown
+        if (progress == null || !contentLength.HasValue) {
+            await download.CopyToAsync(destination, cancellationToken);
+            return;
+        }
+
+        // Convert absolute progress (bytes downloaded) into relative progress (0% - 100%)
+        var relativeProgress = new Progress<long>(totalBytes => progress.Report((float)totalBytes / contentLength.Value));
+        // Use extension method to report progress while downloading
+        await download.CopyToAsync(destination, 81920, relativeProgress, cancellationToken);
+        progress.Report(1);
+    }
+}
+
+public static class StreamExtensions
+{
+    public static async Task CopyToAsync(this Stream source, Stream destination, int bufferSize, IProgress<long> progress = null, CancellationToken cancellationToken = default) {
+        if (source == null)
+            throw new ArgumentNullException(nameof(source));
+        if (!source.CanRead)
+            throw new ArgumentException("Has to be readable", nameof(source));
+        if (destination == null)
+            throw new ArgumentNullException(nameof(destination));
+        if (!destination.CanWrite)
+            throw new ArgumentException("Has to be writable", nameof(destination));
+        if (bufferSize < 0)
+            throw new ArgumentOutOfRangeException(nameof(bufferSize));
+
+        var buffer = new byte[bufferSize];
+        long totalBytesRead = 0;
+        int bytesRead;
+        while ((bytesRead = await source.ReadAsync(buffer, 0, buffer.Length, cancellationToken).ConfigureAwait(false)) != 0) {
+            await destination.WriteAsync(buffer, 0, bytesRead, cancellationToken).ConfigureAwait(false);
+            totalBytesRead += bytesRead;
+            progress?.Report(totalBytesRead);
+        }
+    }
+}
---
+++ AsanaDownloadAttachments/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http.Headers;
+using AsanaClient;
+using Konsole;
+
+const string accessToken = "Your access token here!";
+var client = new HttpClient();
+client.DefaultRequestHeaders.Authorization =  new AuthenticationHeaderValue("Bearer", accessToken);
+var asanaClientWithCredentials = new Client(client);
+var exit = false;
+while (!exit)
+{
+    var projects = await asanaClientWithCredentials.GetProjectsAsync();
+
+    Console.WriteLine("Choose the project you want to download attachments from by typing in the number next to the name");
+    Console.WriteLine(string.Join("\n", projects.Data.Select((x, index) => $"{index + 1} {x.Name}" )));
+
+    var projectNameToDownload = Functions.GetProjectToDownloadFrom(projects);
+    var projectTasks = await asanaClientWithCredentials.GetTasksForProjectAsync(projectNameToDownload.Gid);
+
+    Console.WriteLine("Choose the task you want to download attachments from by typing in the number next to the name");
+    Console.WriteLine(string.Join("\n", projectTasks.Data.Select((x, index) => $"{index + 1} {x.Name}" )));
+
+    var taskToDownloadFrom = Functions.GetTaskToDownloadFrom(projectTasks);
+    var attachmentsResponse = await asanaClientWithCredentials.GetAttachmentsForTaskAsync(taskToDownloadFrom.Gid);
+    Console.WriteLine($"Downloading {attachmentsResponse.Data.Count} attachments.");
+
+    var httpClientWithoutCredentials = new HttpClient();
+    foreach (var attachment in attachmentsResponse.Data)
+    {
+        var attachmentInformation = await asanaClientWithCredentials.GetAttachmentAsync(attachment.Gid);
+        var fileName = attachmentInformation.Data.Name;
+        var tries = 1;
+        while (File.Exists(fileName))
+        {
+            fileName = Path.GetFileNameWithoutExtension(attachmentInformation.Data.Name) + $"({tries})" + Path.GetExtension(attachmentInformation.Data.Name);
+            tries++;
+        }
+        
+        await using var fileStream = File.Create(fileName);
+        var pb = new ProgressBar(PbStyle.SingleLine, 50);
+        var progress = new Progress<float>(f =>
+        {
+            pb.Refresh((int)(f * 100), fileName);
+        });
+        await httpClientWithoutCredentials.DownloadAsync(attachmentInformation.Data.Download_url!.ToString(), fileStream, progress);
+    }
+    await Task.Delay(100);
+    Console.Clear();
+    Console.WriteLine("Finished downloading the attachments. Do you want to download more? Enter:");
+    Console.WriteLine("Yes to start from the beginning.");
+    Console.WriteLine("No to quit the application.");
+    exit = !Functions.GetRepeatDecisionFromUser();
+    Console.Clear();
+}
