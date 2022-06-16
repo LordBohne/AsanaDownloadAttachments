7ff2410de9d84cc202a653b373605f3ed25b9dbc
LordBohne <bohnelord@gmail.com>
2022-06-16 20:45:05 +0200

    Detect missing files

--- AsanaDownloadAttachments/Program.cs
+++ AsanaDownloadAttachments/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
 using AsanaClient;
 using Konsole;
 
-const string accessToken = "Your access token here!";
+const string accessToken = "1/1202356640456951:d8f436b72379beee07fe7ff73882548c";
 var client = new HttpClient();
 client.DefaultRequestHeaders.Authorization =  new AuthenticationHeaderValue("Bearer", accessToken);
 var asanaClientWithCredentials = new Client(client);
 var exit = false;
+var fileRegex = new Regex(@"\.(?<fileEndingNumbers>\d+)$");
 while (!exit)
 {
     var projects = await asanaClientWithCredentials.GetProjectsAsync();
@@ -24,11 +26,20 @@ while (!exit)
     var attachmentsResponse = await asanaClientWithCredentials.GetAttachmentsForTaskAsync(taskToDownloadFrom.Gid);
     Console.WriteLine($"Downloading {attachmentsResponse.Data.Count} attachments.");
 
+    var previousNumberAtEnd = 0;
     var httpClientWithoutCredentials = new HttpClient();
-    foreach (var attachment in attachmentsResponse.Data)
+    foreach (var attachment in attachmentsResponse.Data.OrderBy(x => int.Parse(fileRegex.Match(x.Name).Groups["fileEndingNumbers"].ValueSpan)))
     {
         var attachmentInformation = await asanaClientWithCredentials.GetAttachmentAsync(attachment.Gid);
         var fileName = attachmentInformation.Data.Name;
+        var numberAtEnd = int.Parse(fileRegex.Match(fileName).Groups["fileEndingNumbers"].ValueSpan);
+
+        if (previousNumberAtEnd is not 0 && numberAtEnd - previousNumberAtEnd > 1)
+        {
+            Console.WriteLine($"There seems to be a file missing");
+        }
+
+        previousNumberAtEnd = numberAtEnd;
         var tries = 1;
         while (File.Exists(fileName))
         {
